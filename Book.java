c2e8978c7dc08085ae58e90f1022943724674387
isabelladp <47114084+isabelladp@users.noreply.github.com>
2019-02-04 11:44:38 -0500

    Update Book.java
    -added getPages()

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
      // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
     
     public String getAuthor()
@@ -31,6 +33,10 @@ class Book
     {
         return title;
     }
-
+    
+    public int getPages()
+    {
+        return pages;
+    }
     // Add the methods here ...
 }
